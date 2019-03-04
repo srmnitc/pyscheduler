53a82719bd74addee821bfbccc07cb57cd2437b6
srmnitc <sarath.menon@rub.de>
2019-03-04 07:28:27 +0100

    Add a working slurm scheduler

--- pyscheduler/slurm.py
+++ pyscheduler/slurm.py
@@ -3,6 +3,7 @@ A small scheduler script for submission of jobs.
 
 """
 import os
+import subprocess as sub
 
 class slurm(object):
     def __init__(self, outfile):
@@ -11,12 +12,15 @@ class slurm(object):
         self.header = "#!/bin/bash"
         self.essential = {
             "job-name" : "pyschedule",
+            "partition" : "shorttime",
             "time" : "1:59:00",
             "ntasks" : 4,
             "mem-per-cpu" : "3GB",
             "hint" : "no-multithread"        
         }
 
+        self.output = "slurm.out"
+        self.error = "slurm.err"
         self.modules = []
         self.commands = []
         self.maincommand = []
@@ -31,13 +35,12 @@ class slurm(object):
         dictcopy = self.essential
         self.statements = []
 
+        #process essentials
         for key, vals in dictcopy.iteritems():
             key = "--".join(["",key])
             key = " ".join(["#SBATCH",key])
             skey = "=".join([key,str(vals)])
             self.statements.append(skey)
-
-
 
         with open(self.outfile,"w") as fout:
             #write out the header
@@ -49,11 +52,32 @@ class slurm(object):
                 fout.write(statement)
                 fout.write("\n")
 
+            #write error and output files
+            fout.write(("#SBATCH -o %s\n"),self.output)
+            fout.write(("#SBATCH -e %s\n"),self.error)
+
+            #write modules
+            for module in self.modules:
+                fout.write(" ".join(["module", "load", module]))
+                fout.write("\n")
+
+            #write extra commands
+            for command in self.commands:
+                fout.write(" ".join(command))
+                fout.write("\n")
+
+            #write main command with arg array
+            argarray = " ".join(self.argarray)
+            maincommand = " ".join(self.maincommand) 
+            fout.write(" ".join([maincommand, argarray]))
+
+
     def submit(self):
         """
         Submit job
         """
-        a = 1
+        os.system("sbatch %s", self.script)
+        
 
 
 
